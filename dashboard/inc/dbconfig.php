4821bed512650696390c0a2c5ccd5665bda4e695
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-03-13 21:02:02 +0600

    workwith kufa social

--- dashboard/inc/dbconfig.php
+++ dashboard/inc/dbconfig.php
@@ -1,9 +1,5 @@
 <?php
-const HOST = 'localhost';
-const USER = 'root';
-const PASSWORD = '';
-const DATABASE = 'socials';
-$socialDataBase = new mysqli(HOST, USER, PASSWORD, DATABASE);
+$socialDataBase = new mysqli('localhost', 'root', '', 'kufa');
 if ($socialDataBase->connect_error) :
     die('Connect Error: ' . $socialDataBase->connect_error);
 endif;
--- dashboard/socials.php
+++ dashboard/socials.php
@@ -1,8 +1,52 @@
 <?php
-require_once 'inc/header.php'
+require_once 'inc/header.php';
+$socialView = "SELECT * FROM `socials`";
+if (isset($socialDataBase)) {
+    $socialDataBaseQuery = $socialDataBase->Query($socialView);
+    if ($socialDataBaseQuery) {
+        echo "run";
+    }
+    $socialDataBase->close();
 
+}
 ?>
+<div class="sl-mainpanel">
+    <div class="sl-pagebody">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1> Socials Information</h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-10 m-auto">
+                    <table class=" table table-bordered table-striped text-center table-info">
+                        <tr>
+                            <th>SL</th>
+                            <th>Name</th>
+                            <th>Icon</th>
+                            <th>Link</th>
+                            <th>Status</th>
 
+                        </tr>
+                        <?php
+                        if (isset($socialDataBaseQuery)):
+                            foreach ($socialDataBaseQuery as $index => $social) { ?>
+                                <tr>
+                                    <td><?= ++$index ?></td>
+                                    <td><?= $social['name'] ?></td>
+                                    <td><i class="<?= $social['icon'] ?>"></i></td>
+                                    <td><?= $social['link'] ?></td>
+                                    <td><a data-id="<?= $social['id'] ?>" type="button"
+                                           class="btn btn-warning temporaryDelete">Deactivate</a></td>
+                                </tr>
+                            <?php } endif; ?>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div><!-- sl-pagebody -->
+</div><!-- sl-mainpanel -->
 
 
 <?php
