c0011eb36f3daaf38f71cdd5edac5b088c75954c
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2021-03-15 01:45:43 +0600

    work with kufa social fixed

--- portfolio-single.php
+++ portfolio-single.php
@@ -285,7 +285,7 @@
 
 
 		<!-- JS here -->
-        <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+       <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
         <script src="assets/js/popper.min.js"></script>
         <script src="assets/js/bootstrap.min.js"></script>
         <script src="assets/js/isotope.pkgd.min.js"></script>
