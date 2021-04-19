bc1b6e7a542df20f6f0a715939a6209be9055373
MOHAMMADmiZAN <takbir.jcd@gmail.com>
2021-04-20 00:56:34 +0600

    count setup

--- dashboard/servicesResponse.php
+++ dashboard/servicesResponse.php
@@ -5,7 +5,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $servicesName = $_POST['s-name'];
     $servicesIcon = $_POST['s-icon'];
     $servicesText = $_POST['s-text'];
-    $servicesInsert = "INSERT INTO `services`(`title`,`icon`,`details`) values ('$servicesName','$servicesIcon','$servicesText')";
+
+    $servicesInsert = "INSERT INTO `services`(`title`, `icon`, `details`) values ('$servicesName','$servicesIcon','$servicesText')";
 
     if (isset($kufaDataBase, $servicesInsert)) {
         $servicesInsertQuery = $kufaDataBase->query($servicesInsert);
